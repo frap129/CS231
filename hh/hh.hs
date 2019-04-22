b484bd964dc9a5c294516261644388b4278a8084
Joe Maples <joe@frap129.org>
2019-04-22 13:54:19 -0400

    hh: Add function example
    Signed-off-by: Joe Maples <joe@frap129.org>

--- hh/hh.hs
+++ hh/hh.hs
@@ -1,5 +1,9 @@
+
+hh::String -> String
+hh name = "Hello " ++ name ++ "!"
+
 main = do
     putStrLn "hh: Hello Haskell!"
     putStrLn "What is your name?"
     name <- getLine
-    putStrLn ("Hello " ++ name ++ "!")
+    putStrLn(hh name)
