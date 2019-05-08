ae3b1776d52a02db3f756788e1adcb30563bcbe5
Joe Maples <joe@frap129.org>
2019-05-08 16:53:38 -0400

    lab6: Remove unnecessary parameter from checkTimes
    Signed-off-by: Joe Maples <joe@frap129.org>

--- lab6/HoldMeeting.hs
+++ lab6/HoldMeeting.hs
@@ -1,13 +1,13 @@
 module HoldMeeting where
 
-checkTimes :: [Int] -> Int -> Int -> Int
-checkTimes list length people
-    | length == 2                     = people
-    | list!!(length - 1) <= list!!1   = checkTimes list (length - 1) (people + 1)
-    | otherwise                       = checkTimes list (length - 1) people
+checkTimes :: [Int] -> Int -> Int
+checkTimes list length
+    | length == 2                     = 0
+    | list!!(length - 1) <= list!!1   = 1 + checkTimes list (length - 1)
+    | otherwise                       = checkTimes list (length - 1)
 
 holdMeeting :: String -> (Int, Bool)
 holdMeeting input =
     (onTime, onTime >= head intList) where
     intList = map read (words input)
-    onTime = checkTimes intList (length intList) 0
+    onTime = checkTimes intList (length intList)
