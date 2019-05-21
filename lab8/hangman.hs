9715c484916cf6f24cdd59de491e3d7f53bdd876
Joe Maples <joe@frap129.org>
2019-05-21 16:31:33 -0400

    lab8: Clean code, print a correct word on lose
    Signed-off-by: Joe Maples <joe@frap129.org>

--- lab8/hangman.hs
+++ lab8/hangman.hs
@@ -6,6 +6,9 @@ import Data.List
 import Data.Char
 import qualified Data.Map as Map
 
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+printUsage :: IO a
 printUsage = do
     name <- getProgName
     die ("Usage: ./" ++ name ++ " dictionary_path word_len num_guesses")
@@ -21,27 +24,27 @@ splitArgs :: [String] -> (String, Int, Int, Bool)
 splitArgs [a, b, c, d] = (a, (read b), (read c), (d == "-s"))
 splitArgs [a, b, c, d, e] = (a, (read b), (read c), (d == "-s"))
 
-userView :: String -> String -> String -> String
+userView :: Char -> String -> String -> String
 userView _ _ [] = []
 userView letter (x:xs:xss) (y:ys)
-    | letter!!0 == y = (letter ++ " " ++ userView letter xss ys)
-    | otherwise      = (x : xs : userView letter xss ys)
+    | (toUpper letter) == (toUpper y) = (toUpper letter) : ' ' : userView letter xss ys
+    | otherwise             = x : xs : userView letter xss ys
 
-wordView :: String -> String -> String
-wordView letter word = map (\x -> if elem x letter then x else '_') word
+wordView :: Char -> String -> String
+wordView letter word = map (\x -> if x == letter then x else '_') word
 
-insertFamily ::String -> String -> Map.Map String [String] -> Map.Map String [String]
+insertFamily :: Char -> String -> Map.Map String [String] -> Map.Map String [String]
 insertFamily guessed word = Map.insertWith (++) (wordView guessed word) [word]
 
-wordFamilies :: String -> [String] -> Map.Map String [String]
+wordFamilies :: Char -> [String] -> Map.Map String [String]
 wordFamilies guessed words = foldr (insertFamily guessed) Map.empty words
 
-largestFamily :: String -> [String] -> [String]
+largestFamily :: Char -> [String] -> [String]
 largestFamily guessed words = head $ map snd $ sortByLength $ Map.toList $ wordFamilies guessed words
 
-checkGuess :: String -> [String] -> Bool
+checkGuess :: Char -> [String] -> Bool
 checkGuess guess availWords
-    | elem (guess!!0) (availWords!!0) = True
+    | elem guess (availWords!!0) = True
     | otherwise                       = False
 
 sortByLength :: [(String, [String])] -> [(String, [String])]
@@ -50,26 +53,32 @@ sortByLength a = sortBy (compare `on` length) a
 rightLenWords :: [String] -> Int -> [String]
 rightLenWords a len = [x | x <- a, length x == len]
 
-gameLoop :: [String] -> String -> Int -> IO ()
-gameLoop _ _ 0 = do putStrLn "Sorry, you loose."
-gameLoop availWords userWord guesses
+hiddenString :: Int -> String
+hiddenString len = (concat $ replicate len "_ ")
+
+gameLoop :: [String] -> String -> String -> Int -> Bool -> IO ()
+gameLoop availWords _ _ 0 _ = do putStrLn $ "Sorry, the word was " ++ head availWords
+gameLoop availWords userWord guesses numGuess debug
     | not $ elem '_' userWord = do putStrLn "You win!"
     | otherwise               = do
         putStrLn "Input a guess:"
-        guess <- getLine
+        uncheckedGuess <- getLine
+
+        let guess = uncheckedGuess!!0
         let newWords = largestFamily guess availWords
         let newUserWord = userView guess userWord $ head newWords
-        let newGuesses = if checkGuess guess newWords then guesses else guesses - 1
+        let newNumGuess = if checkGuess guess newWords then numGuess else numGuess - 1
+        let prefix = if checkGuess guess newWords then "Correct! " else "Incorrect! "
+        let newGuesses = userView guess guesses alphabet
 
-        if checkGuess guess newWords then do
-            putStrLn $"Correct! Word: " ++ userView guess userWord (head newWords)
-            putStrLn $ "Guesses remaining: " ++ show guesses
-        else do
-            let newGuesses = guesses - 1
-            putStrLn $ "Incorrect! Word: " ++ userWord
-            putStrLn $ "Guesses remaining: " ++ show (guesses - 1)
-            
-        gameLoop newWords newUserWord newGuesses
+        putStrLn $ prefix ++ "Guesses remaining: " ++ show newNumGuess
+        putStrLn $ "Letters guessed: " ++ newGuesses
+        putStrLn $ "Word so far: " ++ userView guess userWord (head newWords)
+
+        if debug then do putStrLn $ "Words in family: " ++ show (length newWords)
+        else return ()
+
+        gameLoop newWords newUserWord newGuesses newNumGuess debug
 
 main = do
     args <- getArgs
@@ -83,10 +92,11 @@ main = do
     dictContent <- readFile dictName
     let dictWords = rightLenWords (lines dictContent) wordLen
 
+
     if dictWords == [] then do
         die "The dictionary doesn\'t have any words of that length"
     else
         return ()
 
-    gameLoop dictWords (concat $replicate wordLen "_ ") numGuess
+    gameLoop dictWords (hiddenString wordLen) (hiddenString 26) numGuess debug
     return ()
