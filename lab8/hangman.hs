0deb34a69583fbe4d908fd153e95e9abb1226576
Joe Maples <joe@frap129.org>
2019-05-21 13:10:38 -0400

    lab8: Logic works, gameLoop does not
    Signed-off-by: Joe Maples <joe@frap129.org>

--- lab8/hangman.hs
+++ lab8/hangman.hs
@@ -1,22 +1,87 @@
 import System.Environment
 import System.Exit
 import System.IO
+import Data.Function
+import Data.List
+import Data.Char
+import qualified Data.Map as Map
 
 printUsage = do
     name <- getProgName
     die ("Usage: ./" ++ name ++ " dictionary_path word_len num_guesses")
 
-handleArgs :: [String] -> Int
-handleArgs [_, _, _] = 3
-handleArgs [_, _, _, s]
+checkArgs :: [String] -> Int
+checkArgs [_, _, _] = 3
+checkArgs [_, _, _, s]
     | s == "-s" = 4
     | otherwise = 3
-handleArgs _ = 0
+checkArgs _ = 0
+
+splitArgs :: [String] -> (String, Int, Int, Bool)
+splitArgs [x, y, z] = (x, (read y), (read x), False) 
+splitArgs [a, b, c, d] = (a, (read b), (read c), True)
+
+userView :: String -> String -> String -> String
+userView _ _ [] = []
+userView letter (x:xs) (y:ys)
+    | letter!!0 == y = (letter ++ " " ++ userView letter xs ys)
+    | otherwise      = (x : ' ' : userView letter xs ys)
+
+wordView :: String -> String -> String
+wordView letter word = map (\x -> if elem x letter then x else '_') word
+
+insertFamily ::String -> String -> Map.Map String [String] -> Map.Map String [String]
+insertFamily guessed word = Map.insertWith (++) (wordView guessed word) [word]
+
+wordFamilies :: String -> [String] -> Map.Map String [String]
+wordFamilies guessed words = foldr (insertFamily guessed) Map.empty words
+
+largestFamily :: String -> [String] -> [String]
+largestFamily guessed words = head $ map snd $ sortByLength $ Map.toList $ wordFamilies guessed words
+
+checkGuess :: String -> [String] -> Bool
+checkGuess guess availWords
+    | elem (guess!!0) (availWords!!0) = True
+    | otherwise                       = False
+
+sortByLength :: [(String, [String])] -> [(String, [String])]
+sortByLength a = sortBy (compare `on` length) a
+
+rightLenWords :: [String] -> Int -> [String]
+rightLenWords a len = [x | x <- a, length x == len]
+
+gameLoop :: [String] -> String -> Int -> String
+gameLoop _ _ 0 = "Sorry, you loose."
+gameLoop availWords userWord guesses
+    | not $ elem '-' userWord = "You win!"
+    | otherwise             = do
+        putStrLn "Input a guess:"
+        guess <- getLine
+        let newWords = largestFamily guess availWords
+        if checkGuess guess newWords then do
+            putStrLn "Correct! Word: " ++ userView guess userWord (head newWords)
+            putStrLn "Guesses remaining: " ++ show guesses
+            gameLoop newWords (userView guess userWord head (newWords)) guesses
+        else
+            putStr "Incorrect! Word: " ++ userWord
+            putStrLn "Guesses remaining: " ++ show (guesses - 1)
+            gameLoop (largestFamily guess availWords) userWord (guesses - 1)
 
 main = do
     args <- getArgs
-    let numArgs = handleArgs args
-    if (numArgs == 0) then do
+    let numArgs = checkArgs args
+    if numArgs == 0 then do
         printUsage
     else
-        print numArgs -- Temporary
+        return ()
+
+    let (dictName, wordLen, numGuess, debug) = splitArgs args 
+    dictContent <- readFile dictName
+    let dictWords = rightLenWords (lines dictContent) wordLen
+
+    if dictWords == [] then do
+        die "The dictionary doesn\'t have any words of that length"
+    else
+        return ()
+
+    gameLoop dictWords (replicate wordLen '_') numGuess
